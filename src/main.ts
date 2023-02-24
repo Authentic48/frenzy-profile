78be1e646d4211ef6a46dae81e427393ed6a98ff
Fall Ibrahim <ibrahim_fall@smartworld.team>
2023-02-24 21:06:58 +0700

    feat: remove global api prefix

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,6 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api');
-
   const configService = app.get<ConfigService>(ConfigService);
   const port = parseInt(configService.get('APP_PORT'));
 
